e6d79614d09c023f7f6c1acd9dd0c8c649ca6594
Subhasis Das <subhasisdas125@gmail.com>
2020-02-09 13:32:05 +0530

    added spinner

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const [file] = document.getElementById("file").files;
+  if (!file) {
+    alert("Please select a file!");
+  } else {
+    let imageHolder = document.getElementById("imageHolder");
+    imageHolder.innerHTML = `
+    <img src="./images/${file.name}" id="img" alt="demopic" />
+    `;
+
+    setPrediction(document.getElementById("img"));
+  }
+});
+
+function spinner(loading) {
+  loading &&
+    (document.getElementById("predictionTable").innerHTML = `
+  <img src="./images/spinner.gif" alt="spinner" />
+  `);
+}
+
 async function predictLabel(img) {
   const model = await mobilenet.load();
   return await model.classify(img);
 }
 
 async function setPrediction(img) {
+  spinner(true);
+
   const prediction = await predictLabel(img);
 
   let outputMarkup = `
@@ -22,26 +48,11 @@ async function setPrediction(img) {
   });
 
   //DOM manipulation
+  spinner(false);
+
   document
     .getElementById("predictionTable")
     .setAttribute("style", "display:'inline-block';");
 
   document.getElementById("predictionTable").innerHTML = outputMarkup;
 }
-
-const form = document.getElementById("form");
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const [file] = document.getElementById("file").files;
-  if (!file) {
-    alert("Please select a file!");
-  } else {
-    let imageHolder = document.getElementById("imageHolder");
-    imageHolder.innerHTML = `
-    <img src="./images/${file.name}" id="img" alt="demopic" />
-    `;
-
-    setPrediction(document.getElementById("img"));
-  }
-});
